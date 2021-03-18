2fb41fc631c5c5026726caf4e319fa87b5a98466
Majid Parvin <parvin.majid@live.com>
2021-03-18 12:59:33 +0100

    introduce [put] api/lead/accept/{id:int} endpoint

--- server/Lead.Management/Lead.Management.API/Controllers/LeadController.cs
+++ server/Lead.Management/Lead.Management.API/Controllers/LeadController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Lead.Management.Application.Handlers.Leads;
+using Lead.Management.Application.Handlers.Leads.Commands;
 using Lead.Management.Application.Handlers.Leads.Queries;
 using MediatR;
 namespace Lead.Management.API.Controllers
@@ -17,15 +18,26 @@ namespace Lead.Management.API.Controllers
 
 
         [HttpGet("invited")]
+        [ProducesResponseType(200)]
         public async Task<IEnumerable<InvitedLeadDto>> GetInvitedLeads(CancellationToken cancellationToken)
         {
             return await _mediator.Send(new GetInvitedLeads.Query(), cancellationToken);
         }
 
         [HttpGet("accepted")]
+        [ProducesResponseType(200)]
         public async Task<IEnumerable<InvitedLeadDto>> GetAcceptedLeads(CancellationToken cancellationToken)
         {
             return await _mediator.Send(new GetAcceptedLead.Query(), cancellationToken);
+        }
+
+        [HttpPut("accept/{id:int}")]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult> AcceptLead(int id, CancellationToken cancellationToken)
+        {
+             await _mediator.Send(new AcceptLead.Command(id), cancellationToken);
+
+             return Ok();
         }
     }
 }
---
+++ server/Lead.Management/Lead.Management.Application/Handlers/Leads/Commands/AcceptLead.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Lead.Management.Application.Services.Interfaces;
+using MediatR;
+
+namespace Lead.Management.Application.Handlers.Leads.Commands
+{
+    public class AcceptLead
+    {
+        public class Command : IRequest
+        {
+            public int Id { get; }
+
+            public Command(int id)
+            {
+                Id = id;
+            }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly ILeadManagementRepository _leadManagementRepository;
+            public Handler(ILeadManagementRepository leadManagementRepository)
+            {
+                _leadManagementRepository = leadManagementRepository;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                await _leadManagementRepository.AcceptLeadByIdAsync(request.Id, cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
