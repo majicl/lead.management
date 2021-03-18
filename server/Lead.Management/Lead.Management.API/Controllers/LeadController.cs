0936b1341835fa73eaf76271ded99e0d2db22014
Majid Parvin <parvin.majid@live.com>
2021-03-18 12:27:38 +0100

    Introduce API project with /api/lead/invited endpoint

---
+++ server/Lead.Management/Lead.Management.API/Controllers/LeadController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Lead.Management.Application.Handlers.Leads;
+using Lead.Management.Application.Handlers.Leads.Queries;
+using MediatR;
+namespace Lead.Management.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeadController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IMediator _mediator;
+
+        public LeadController(IMediator mediator) => _mediator = mediator;
+
+
+        [HttpGet("invited")]
+        public async Task<IEnumerable<InvitedLeadDto>> GetInvitedLeads(CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(new GetInvitedLeads.Query(), cancellationToken);
+        }
+    }
+}
