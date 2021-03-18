111b414a4d6025759e00e2b07f99f8cd48bdd8ee
Majid Parvin <parvin.majid@live.com>
2021-03-18 22:13:10 +0100

    fix date and time format

--- server/Lead.Management/Lead.Management.Application/Extensions/DateTimeExtensions.cs
+++ server/Lead.Management/Lead.Management.Application/Extensions/DateTimeExtensions.cs
@@ -6,12 +6,12 @@ namespace Lead.Management.Application.Extensions
     {
         public static string GetDate(this DateTime date)
         {
-            return date.ToUniversalTime().ToShortDateString();
+            return date.ToUniversalTime().ToString("MMMM dd");
         }
 
         public static string GetTime(this DateTime date)
         {
-            return date.ToUniversalTime().ToShortTimeString();
+            return date.ToUniversalTime().ToString("hh:mm tt");
         }
     }
 }
