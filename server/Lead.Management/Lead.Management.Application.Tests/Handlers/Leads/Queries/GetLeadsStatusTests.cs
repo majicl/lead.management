d1990218e22e3932dcc683aacd77a41bfe990619
Majid Parvin <parvin.majid@live.com>
2021-03-20 17:53:12 +0100

    add GetLeadsStatusTests unit-tests 🔱

---
+++ server/Lead.Management/Lead.Management.Application.Tests/Handlers/Leads/Queries/GetLeadsStatusTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Lead.Management.Application.Handlers.Leads.Queries;
+using Lead.Management.Application.Services.Interfaces;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Lead.Management.Application.Tests.Handlers.Leads.Commands
+{
+    public class GetLeadsStatusTests
+    {
+
+        private readonly Mock<ILeadManagementRepository> _leadManagementRepository;
+        private readonly GetLeadsStatus.Handler _target;
+
+        public GetLeadsStatusTests()
+        {
+            var mocker = new AutoMocker();
+            _leadManagementRepository = mocker.GetMock<ILeadManagementRepository>();
+            _target = new GetLeadsStatus.Handler(_leadManagementRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_Calls_GetLeadUpdateAsync()
+        {
+            //Arrange 
+            _leadManagementRepository
+                .Setup(_ => _.GetLeadUpdateAsync(CancellationToken.None));
+
+            //Act
+            var query = new GetLeadsStatus.Query();
+            await _target.Handle(query, CancellationToken.None);
+
+            //Assert
+            _leadManagementRepository.Verify(_ => _.GetLeadUpdateAsync(CancellationToken.None), Times.Once);
+        }
+    }
+}
