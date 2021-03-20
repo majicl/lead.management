dd5a7564899ad8e343c9d09003a9bf58b08a5ae4
Majid Parvin <parvin.majid@live.com>
2021-03-20 17:40:35 +0100

    exclude coverage folder from git

---
+++ server/Lead.Management/Lead.Management.Application.Tests/Handlers/Leads/Queries/GetAcceptedLeadTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Lead.Management.Application.Handlers.Leads.Queries;
+using Lead.Management.Application.Services.Interfaces;
+using Lead.Management.Domain;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Lead.Management.Application.Tests.Handlers.Leads.Commands
+{
+    public class GetAcceptedLeadTests
+    {
+
+        private readonly Mock<ILeadManagementRepository> _leadManagementRepository;
+        private readonly GetAcceptedLead.Handler _target;
+
+        public GetAcceptedLeadTests()
+        {
+            var mocker = new AutoMocker();
+            _leadManagementRepository = mocker.GetMock<ILeadManagementRepository>();
+            _target = new GetAcceptedLead.Handler(_leadManagementRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_Calls_GetAcceptedLeadsAsync()
+        {
+            //Arrange 
+            var query = new GetAcceptedLead.Query();
+
+            //Act
+            await _target.Handle(query, CancellationToken.None);
+
+            //Assert
+            _leadManagementRepository.Verify(_ => _.GetAcceptedLeadsAsync(CancellationToken.None), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_Maps_Data()
+        {
+            //Arrange
+            var lead = new AcceptedLead
+            {
+                Id = 1,
+                Area = "Sydney",
+                Category = "Painter",
+                ContactEmail = "test@test.com",
+                ContactName = "Alex Nouri",
+                ContactPhone = "893398498",
+                CreatedAt = new System.DateTime(2020, 03, 20, 18, 20, 22),
+                Description = "descp",
+                Postcode = "1234",
+                Price = 1290
+
+            };
+            var query = new GetAcceptedLead.Query();
+            _leadManagementRepository
+                .Setup(_ => _.GetAcceptedLeadsAsync(CancellationToken.None))
+                .ReturnsAsync(new List<AcceptedLead> { lead });
+
+            //Act
+            var result = await _target.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.Single(result);
+            var mappedLead = result.First();
+
+            Assert.Equal(lead.Id, mappedLead.Id);
+            Assert.Equal(lead.Category, mappedLead.Category);
+            Assert.Equal(lead.ContactEmail, mappedLead.ContactEmail);
+            Assert.Equal(lead.ContactPhone, mappedLead.ContactPhone);
+            Assert.Equal("Alex", mappedLead.ContactFirstName);
+            Assert.Equal("Nouri", mappedLead.ContactLastName);
+            Assert.Equal("06:20PM", mappedLead.CreatedAtTime);
+            Assert.Equal("March 20", mappedLead.CreatedAtDate);
+            Assert.Equal(lead.Description, mappedLead.Description);
+        }
+    }
+}
